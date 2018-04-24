cf4a4503a95c116b001132d139033c0323fbb9e0
Saiyi.Li <Saiyi.Li@myob.com>
2018-04-24 11:41:12 +1000

    Create a simple project with calculator class
    A test for the calculator class is added

---
+++ SimpleProjects/SimpleProjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SimpleProjects/SimpleProjectsTests/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleProjects;
+
+namespace SimpleProjectsTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Sum_GivenTwoNumbers_ShouldReturnCorrectResult()
+        {
+            // Arrange
+            int num1 = 1;
+            int num2 = 3;
+            int expected = 4;
+            Calculator calculator = new Calculator();
+            
+            // Act
+            int actual = calculator.Sum(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
